c13e5f3100fe9d156adfff8c65ef34f8f941dc60
Christian Downs <christian.downs.15@gmail.com>
2024-02-23 22:51:59 -0800

    WIP fixing up booking

--- src/views/Customer/Book.js
+++ src/views/Customer/Book.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CustomCalendar from 'components/Calendar/CustomCalendar';
 import dayjs from 'dayjs';
 import { Col, Row } from 'react-bootstrap';
@@ -6,14 +6,17 @@ import axios from 'axios';
 import 'assets/css/booking.css';
 
 const BookingPage = ({ theme }) => {
-    console.log(theme.id)
+    // console.log(theme.id)
 
-    const [value, setValue] = React.useState(dayjs());
+    const [selectedDate, setSelectedDate] = React.useState(dayjs());
 
     // const [theme, setTheme] = useState('light');
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
+    const [streetAddress, setStreetAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [zipcode, setZipcode] = useState('');
 
     // const handleThemeChange = (event) => {
     //     setTheme(event.target.value);
@@ -31,16 +34,65 @@ const BookingPage = ({ theme }) => {
         setPhoneNumber(event.target.value);
     };
 
+    const handleStreetAddressChange = (event) => {
+        setStreetAddress(event.target.value);
+    }
+
+
+    const validateForm = () => {
+        var validEmail = false
+
+        if (name == '' || email == "" || phoneNumber == "") {
+            alert("All fields must be filled out")
+            return false;
+        }
+
+        var emailRegex = /\S+@\S+\.\S+/;
+        if (!emailRegex.test(email)) {
+            alert("Invalid email address");
+            return false;
+        }
+
+        const phoneRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
+        if (!phoneRegex.test(phoneNumber)) {
+            alert("Invalid Phone Number")
+            return false;
+        }
+
+        if (selectedDate) {
+            console.log(selectedDate.toDate().getDate() + " " + (selectedDate.toDate().getMonth() + 1) + " " + selectedDate.toDate().getFullYear())
+        } else {
+            console.log("No selected Date")
+            return false
+        }
+
+        return true
+    }
+
     const handleBooking = () => {
         // Handle booking logic here
         console.log('Booking now...');
+        if (validateForm()) {
+            axios.put(process.env.REACT_APP_SERVER_URL + "/order", {
+
+            })
+        } else {
+            console.log("Invalid")
+        }
+
+
+
     };
 
-    axios.get(process.env.REACT_APP_SERVER_URL + "/api/calendar").then(function (response) {
-        console.log(response.data)
-    }).catch(function (error) {
-        console.log(error)
-    })
+    useEffect(() => {
+        axios.get(process.env.REACT_APP_SERVER_URL + "/api/calendar/"+theme.id).then(function (response) {
+            console.log(response.data)
+        }).catch(function (error) {
+            console.log(error)
+        })
+    }, [])
+
 
 
 
@@ -49,7 +101,7 @@ const BookingPage = ({ theme }) => {
         new Date('2024-02-11'),
     ];
 
-    console.log(disabledDates)
+    // console.log(disabledDates)
 
 
 
@@ -60,8 +112,8 @@ const BookingPage = ({ theme }) => {
                 {/* For the calendar column, ensure it's centered on small screens */}
                 <Col md={6} className='booking-left-col d-flex align-items-center '>
                     <CustomCalendar
-                        value={value}
-                        setValue={setValue}
+                        value={selectedDate}
+                        setValue={setSelectedDate}
                         disabledDates={disabledDates}
                     />
                 </Col>
@@ -93,8 +145,12 @@ const BookingPage = ({ theme }) => {
                         </Col>
                     </Row>
                     <Row className='d-flex  booking-right-col-row'>
+                        <label for="streetAddress">Street Address:</label>
+                        <input type='text' id="streetAddress" value={streetAddress} onChange={handleStreetAddressChange} />
+                    </Row>
+                    <Row className='d-flex  booking-right-col-row'>
                         <Col xs={12} md={8} className='button-col'>
-                            <button type="button" className = "booking-button" onClick={handleBooking}>Book Now</button>
+                            <button type="button" className="booking-button" onClick={handleBooking}>Book Now</button>
                         </Col>
                     </Row>
                 </Col>
