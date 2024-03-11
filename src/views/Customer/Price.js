726a31b7b16f8ce7c74ff5eb4bf7ca94362e22cf
Christian Downs <christian.downs.15@gmail.com>
2024-03-11 02:39:43 -0500

    Finished pricing html css

--- src/views/Customer/Price.js
+++ src/views/Customer/Price.js
@@ -1,17 +1,22 @@
 import React, { useEffect } from "react";
-import { Col, Image, Row } from "react-bootstrap";
+import { Button, Col, Image, Row } from "react-bootstrap";
 import "assets/css/price.css";
 import "assets/css/addon-card.css";
 import website from "assets/jsons/website.json";
+import ribbon from "assets/img/price/ribbon.png";
+import { useNavigate } from "react-router-dom";
 
 const Price = () => {
   const price_page = website.price_page;
   const includedSection = price_page.included;
-  const includedItems = price_page.included.Prices;
+  const includedItems = includedSection.items;
+  const prices = price_page.included.Prices;
   const addons = price_page.Addons;
-  console.log(addons);
-  console.log(price_page);
-  console.log(includedItems);
+ const navigate = useNavigate();
+  const bookNowButtonHandler = () => {
+    navigate("/book", {});
+  };
+
   return (
     <div>
       <Row className="top-level-row">
@@ -23,40 +28,104 @@ const Price = () => {
             <h1 className="price-header">{price_page.header.large_text}</h1>
             <p className="price-subheader">{price_page.header.small_text}</p>
           </div>
+          <div className="book-now-button-div">
+            <Button className="book-now-button" onClick={bookNowButtonHandler}>
+              Book Now
+            </Button>
+          </div>
         </div>
       </Row>
-      <Row
+
+      <Col
         className="party-pricing-info"
-        style={{ background: includedSection.background }}
+        // style={{ background: includedSection.background }}
       >
-        <div className="party=pricing-info-div-holder">
-          <Row className="party-pricing-title-row">
-            <p className="party-pricing-title">{includedSection.header}</p>
-          </Row>
-          <Col className="party-pricing-col">
-            {Object.keys(includedItems).map((price) => {
-              return (
-                <Row className="price-holder">
-                  <span className="price-title">{price}</span>
-                  -
-                  <span className="price-price">{includedItems[price]}</span>
-                </Row>
-              );
-            })}
-          </Col>
+        <div className="party-pricing-title-div">
+          <div className="party-pricing-title-image-div">
+            <Image className="party-pricing-title-image" src={ribbon} />
+            <h1 className="party-pricing-title">{includedSection.header}</h1>
+          </div>
+          <div className="party-pricing-subtext-div">
+            <p className="party-pricing-subtext">{includedSection.subHeader}</p>
+          </div>
         </div>
-      </Row>
+
+        <Row className="party-info-row desktop">
+          <div className="party-info-divider">
+            <div className="party-pricing-info-col">
+              <div className="party-pricing-info-div-holder">
+                <h2>Party Packages:</h2>
+                {Object.keys(prices).map((price) => {
+                  return (
+                    <div className="party-pricing-info-div">
+                      <p className="party-pricing-info-text">
+                        {price}- {prices[price]}
+                      </p>
+                      <p className="party-pricing-info-text"></p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+
+          <div className="party-info-divider">
+            <div className="party-included-col">
+              <div className="party-included-div">
+                <h2 className="included-header">Included:</h2>
+                <ul className="included-list">
+                  {Object.keys(includedItems).map((item) => {
+                    return (
+                      <div className="item-holder">
+                        <li className="included-item">{includedItems[item]}</li>
+                      </div>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </Row>
+
+        <Row className="party-info-row mobile">
+          <div className="party-info-divider">
+            <div className="party-pricing-info-col">
+              <div className="party-pricing-info-div-holder mobile">
+                Party Packages:
+                {Object.keys(prices).map((price) => {
+                  return (
+                    <div className="party-pricing-info-div">
+                      <p className="party-pricing-info-text mobile">
+                        {price}- {prices[price]}
+                      </p>
+                      <p className="party-pricing-info-text"></p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+
+          <div className="party-info-divider">
+            <div className="party-included-col">
+              <div className="party-included-div">
+                <h2 className="included-header">Included:</h2>
+                <ul className="included-list">
+                  {Object.keys(includedItems).map((item) => {
+                    return (
+                      <li className="included-item">{includedItems[item]}</li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </Row>
+      </Col>
       <Col className="addon-col">
-      
         {Object.keys(addons.items).map((addon) => {
           return AddonCard(addons.items[addon]);
         })}
-        {/* {Object.keys(addons.items).map((addon) => {
-          return AddonCard(addons.items[addon]);
-        })}
-        {Object.keys(addons.items).map((addon) => {
-          return AddonCard(addons.items[addon]);
-        })} */}
       </Col>
     </div>
   );
@@ -68,17 +137,11 @@ const imageContext = require.context(
   /\.(png|jpe?g|svg)$/
 );
 
-
-
-
 const AddonCard = (addon) => {
   const [image, setImage] = React.useState("");
   const [style, setStyle] = React.useState({ display: "none" });
 
-
   const modalId = "modal" + addon.name;
-
-
 
   useEffect(() => {
     // imageContext(`./${addon.image}`).then((image) => {
@@ -88,26 +151,25 @@ const AddonCard = (addon) => {
   }, []);
 
   const openModal = () => {
-      setStyle({ display: "block" });
-  }
+    setStyle({ display: "block" });
+  };
 
   const closeModal = () => {
     setStyle({ display: "none" });
-  }
+  };
 
   return (
     <div className="addon-card">
       <div className="addon-card-header">
-        <p className="addon-card-title">
-          {addon.name}
-          
-        </p>
+        <p className="addon-card-title">{addon.name}</p>
         <p className="addon-card-price">${addon.price}</p>
       </div>
       <div className="addon-card-body">
-        <Image className="addon-card-image" src={image} onClick={openModal}/>
+        <Image className="addon-card-image" src={image} onClick={openModal} />
         <div id={modalId} className="modal" style={style}>
-          <span class="close" onClick={closeModal}>&times;</span>
+          <span class="close" onClick={closeModal}>
+            &times;
+          </span>
           <Image src={image} className="modal-content" id="img01" />
           <div id="caption">{addon.name}</div>
         </div>
